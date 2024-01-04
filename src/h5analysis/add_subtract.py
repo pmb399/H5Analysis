23084beed3ce4e215e4b5b2b4554f11354a77532
pmb399 <web@braunpatrick.de>
2024-01-04 14:06:05 -0600

    Review and Doc "add_subtract"

--- src/h5analysis/add_subtract.py
+++ src/h5analysis/add_subtract.py
@@ -1,13 +1,32 @@
+# Scientific modules
 import numpy as np
 from scipy.interpolate import interp1d, interp2d
+
+# Data loaders
 from .data_1d import load_1d
 from .data_2d import load_2d
 from .histogram import load_histogram
+
+# Utilities
 from .simplemath import apply_offset, apply_savgol, grid_data2d, grid_data, bin_data, grid_data_mesh
 from .readutil import detector_norm
+
+# Warnings
 import warnings
 
 def ScanAddition(config,file, x_stream, y_stream, *args, norm=False, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None, grid_x=[None, None, None], savgol=None, binsize=None, legend_item=None):
+    """Internal function to handle scan addition.
+
+        Parameters
+        ----------
+        args: Same as for the Load1d class
+        kwargs: See Load1d class
+
+        Returns
+        -------
+        data: dict
+    """
+
     # Define generic object in which all data will be stored
     class added_object:
         def __init__(self):
@@ -18,9 +37,10 @@ def ScanAddition(config,file, x_stream, y_stream, *args, norm=False, xoffset=Non
         if args.count(i) > 1:
             raise ValueError("Cannot add the same scan to itself")
 
+    # Load all specified scan data
     ScanData = load_1d(config,file, x_stream, y_stream, *args, norm=False, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None, grid_x=[None, None, None], savgol=None, binsize=None)
 
-        # Iterate over all loaded scans
+    # Iterate over all loaded scans
     for i, (k, v) in enumerate(ScanData.items()):
         # Set the first scan as master data
         if i == 0:
@@ -90,11 +110,29 @@ def ScanAddition(config,file, x_stream, y_stream, *args, norm=False, xoffset=Non
     return data
 
 def ScanSubtraction(config,file, x_stream, y_stream, minuend, subtrahend, norm=False, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None, grid_x=[None, None, None], savgol=None, binsize=None, legend_item=None):
+    """ Internal function to handle scan subtraction.
+
+        Parameters
+        ----------
+        args: Same as for the Load1d class, but
+        minuend: list
+            adds all scans in list, generates minuend
+        subtrahend: list
+            adds all scans in list, generates subtrahend
+        kwargs: See Load1d class
+
+        Returns
+        -------
+        data: dict
+    """
+    
     # Define generic object in which all data will be stored
     class added_object:
         def __init__(self):
             pass
-
+        
+    # Get the minuend and subtrahend data
+    # Pass the scans specified in each list to the Scan addition function
     minuendData = ScanAddition(config,file, x_stream, y_stream, *minuend, norm=False, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None, grid_x=[None, None, None], savgol=None, binsize=None)
     subtrahendData = ScanAddition(config,file, x_stream, y_stream, *subtrahend, norm=False, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None, grid_x=[None, None, None], savgol=None, binsize=None)
 
@@ -164,6 +202,17 @@ def ScanSubtraction(config,file, x_stream, y_stream, minuend, subtrahend, norm=F
     return data
 
 def ImageAddition(config, file, x_stream, detector, *args, norm=True, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None,grid_x=[None, None, None],grid_y=[None, None,None],norm_by=None):
+    """Internal function to handle image addition.
+
+            Parameters
+            ----------
+            args: Same as for the Load2d class
+            kwargs: See Load2d class
+
+            Returns
+            -------
+            data: dict
+        """
 
    # Define generic object in which all data will be stored
     class added_object:
@@ -175,7 +224,9 @@ def ImageAddition(config, file, x_stream, detector, *args, norm=True, xoffset=No
         if args.count(i) > 1:
             raise ValueError("Cannot add the same scan to itself")
         
-    ScanData = load_2d(config, file, x_stream, detector, *args, norm=False, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None,grid_x=grid_x,grid_y=grid_y,norm_by=None,)
+    # Load all 2d data to be added
+    # Note that this is possible since load2d supports loading multiple scans
+    ScanData = load_2d(config, file, x_stream, detector, *args, norm=False, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None,grid_x=grid_x,grid_y=grid_y,norm_by=norm_by,)
 
     # Iterate over all loaded scans
     for i, (k, v) in enumerate(ScanData.items()):
@@ -213,15 +264,8 @@ def ImageAddition(config, file, x_stream, detector, *args, norm=True, xoffset=No
     # Apply x offset
     data[0].new_x = apply_offset(data[0].new_x, xoffset, xcoffset)
 
-    # Assign the calculated result to the y_stream of the object in data dict
+    # Apply y offset
     data[0].new_y = apply_offset(data[0].new_y, yoffset, ycoffset)
-
-    # Normalize MCA data by SCA
-    if not isinstance(norm_by,type(None)):
-        norm_data = data[0].Scan(norm_by)
-        normalization = norm_data[norm_by]
-
-        data[0].detector = detector_norm(data[0].detector,normalization)
 
     # Normalize data to [0,1]
     if norm == True:
@@ -231,16 +275,36 @@ def ImageAddition(config, file, x_stream, detector, *args, norm=True, xoffset=No
 
 def ImageSubtraction(config, file, x_stream, detector, minuend, subtrahend, norm=True, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None,grid_x=[None, None, None],grid_y=[None, None,None],norm_by=None):
 
+    """ Internal function to handle image subtraction.
+
+        Parameters
+        ----------
+        args: Same as for the Load1d class, but
+        minuend: list
+            adds all images in list, generates minuend
+        subtrahend: list
+            adds all images in list, generates subtrahend
+        kwargs: See Load1d class
+
+        Returns
+        -------
+        data: dict
+    """
+
    # Define generic object in which all data will be stored
     class added_object:
         def __init__(self):
             pass
-        
-    minuend = ImageAddition(config, file, x_stream, detector, *minuend, norm=False, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None,grid_x=grid_x,grid_y=grid_y,norm_by=None,)
-    subtrahend = ImageAddition(config, file, x_stream, detector, *subtrahend, norm=False, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None,grid_x=grid_x,grid_y=grid_y,norm_by=None,)
+    
+    # Define minuend and subtrahend
+    # Add images of all scans specified in respective lists,
+    # then subtract
+    minuend = ImageAddition(config, file, x_stream, detector, *minuend, norm=False, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None,grid_x=grid_x,grid_y=grid_y,norm_by=norm_by,)
+    subtrahend = ImageAddition(config, file, x_stream, detector, *subtrahend, norm=False, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None,grid_x=grid_x,grid_y=grid_y,norm_by=norm_by,)
 
     name = f"{minuend}-{subtrahend}"
 
+    # Set the master streams
     MASTER_x_stream = minuend[0].new_x
     MASTER_y_stream = minuend[0].new_y
     MASTER_detector = minuend[0].new_z
@@ -249,6 +313,7 @@ def ImageSubtraction(config, file, x_stream, detector, minuend, subtrahend, norm
     MASTER_ymin = minuend[0].ymin
     MASTER_ymax = minuend[0].ymax
 
+    # Interpolate the subtrahend onto minuend, then subtract
     interp = interp2d(subtrahend[0].new_x,subtrahend[0].new_y,subtrahend[0].new_z)
     new_z = interp(MASTER_x_stream,MASTER_y_stream)
 
@@ -270,15 +335,8 @@ def ImageSubtraction(config, file, x_stream, detector, minuend, subtrahend, norm
     # Apply x offset
     data[0].new_x = apply_offset(data[0].new_x, xoffset, xcoffset)
 
-    # Assign the calculated result to the y_stream of the object in data dict
+    # Apply y offset
     data[0].new_y = apply_offset(data[0].new_y, yoffset, ycoffset)
-
-    # Normalize MCA data by SCA
-    if not isinstance(norm_by,type(None)):
-        norm_data = data[0].Scan(norm_by)
-        normalization = norm_data[norm_by]
-
-        data[0].detector = detector_norm(data[0].detector,normalization)
 
     # Normalize data to [0,1]
     if norm == True:
@@ -287,6 +345,18 @@ def ImageSubtraction(config, file, x_stream, detector, minuend, subtrahend, norm
     return data
 
 def HistogramAddition(config,file, x_stream, y_stream, z_stream, *args, norm=False):
+
+    """Internal function to handle histogram addition.
+
+            Parameters
+            ----------
+            args: Same as for the Load2d class
+            kwargs: See Load2d class
+
+            Returns
+            -------
+            data: dict
+        """
 
     # Define generic object in which all data will be stored
     class added_object:
@@ -297,10 +367,12 @@ def HistogramAddition(config,file, x_stream, y_stream, z_stream, *args, norm=Fal
     for i in args:
         if args.count(i) > 1:
             raise ValueError("Cannot add the same scan to itself")
-        
+    
+    # Load all histograms, corresponding to the specified scans
     ScanData = load_histogram(config, file, x_stream, y_stream, z_stream, *args, norm=False, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None)
 
     # Iterate over all loaded scans
+    # Append all data arrays to the following lists
     x_data = list()
     y_data = list()
     z_data = list()
@@ -309,12 +381,15 @@ def HistogramAddition(config,file, x_stream, y_stream, z_stream, *args, norm=Fal
         y_data.append(v.y_data)
         z_data.append(v.z_data)
 
+    # Combine the individual data arrays
     all_x = np.concatenate(tuple(x_data))
     all_y = np.concatenate(tuple(y_data))
     all_z = np.concatenate(tuple(z_data))
     
+    # Mesh the data and generate 2d histogram
     xmin, xmax, ymin, ymax, xedge, yedge, new_z, zmin, zmax = grid_data_mesh(all_x,all_y,all_z)
 
+    # Store data
     data = dict()
     data[0] = added_object()
     data[0].xmin = xmin
