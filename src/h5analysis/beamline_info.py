beaa7befad29ff8a890828f977604ea6c35728e2
pmb399 <web@braunpatrick.de>
2024-01-04 14:17:27 -0600

    Review and Doc "beamline_info"

--- src/h5analysis/beamline_info.py
+++ src/h5analysis/beamline_info.py
@@ -1,11 +1,38 @@
+# Scientific modules
 import numpy as np
 import pandas as pd
+
+# Scan Info Loader
 from .ReadData import ScanInfo
+
+# Util functions
 from .simplemath import apply_offset
 from .util import invert_dict
 
-
 def load_beamline(config, file, key, norm=False, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None, legend_item=None):
+    """Load beamline meta data.
+
+        Parameters
+        ----------
+        config: dict,
+            h5 data configuration
+        file: string
+            file name
+        key: string
+            path to the meta data of interest
+        kwargs: See Load1d class, but additional
+            xoffset: fitting offset
+            yoffset: fitting offset
+            xcoffset: constant offset
+            ycoffset: constant offset
+            legend_item: string
+
+        Returns
+        -------
+        data: dict
+    """
+
+    # Init the data storage
     class beamline_object:
         def __init__(self):
             pass
@@ -13,10 +40,14 @@ def load_beamline(config, file, key, norm=False, xoffset=None, xcoffset=None, yo
     data = dict()
     data[0] = beamline_object()
 
+    # Get the scan information from file
     infoObj = ScanInfo(config,file,key)
     info = infoObj.info_dict
+
+    # Parse data to numpy array
     info_array = np.array(list(info[key].items()), dtype='float')
 
+    # Store the data
     data[0].x_stream = info_array[:, 0]
     data[0].y_stream = info_array[:, 1]
     data[0].scan = key
@@ -40,12 +71,29 @@ def load_beamline(config, file, key, norm=False, xoffset=None, xcoffset=None, yo
 
 
 def get_single_beamline_value(config, file, keys, *args):
+    """Load beamline meta data.
+
+        Parameters
+        ----------
+        config: dict,
+            h5 data configuration
+        file: string
+            file name
+        keys: string, list
+            path to the meta data of interest
+        args: int
+            scan numbers, comma separated
+    """
+
+    # Get the meta data
     infoObj = ScanInfo(config, file, keys)
     info = infoObj.info_dict
 
+    # Check type of key, need it as list
     if not isinstance(keys, list):
         keys = [keys]
 
+    # Print the results to screen
     for entry in keys:
         print('=====  ', entry, '  =====')
 
@@ -57,10 +105,24 @@ def get_single_beamline_value(config, file, keys, *args):
 
 
 def get_spreadsheet(config, file, columns=None):
+    """Generate spreadsheet with meta data from h5 file.
+
+        Parameters
+        ----------
+        config: dict,
+            h5 data configuration
+        file: string
+            file name
+        columns: dict
+            Specify column header and h5 data path to meta datam i.e.
+                columns = dict()
+                columns['Sample Stage horz'] = 'Endstation/Motors/ssh
+                ...
+    """
+        
     if columns == None:
         columns = dict()
 
-        #columns['Sample'] = 'command'
         columns['Command'] = 'command'
         columns['Sample Stage (ssh)'] = 'Endstation/Motors/ssh'
         columns['Sample Stage (ssv)'] = 'Endstation/Motors/ssv'
@@ -71,9 +133,11 @@ def get_spreadsheet(config, file, columns=None):
         columns['Mono Grating'] = '/Beamline/Monochromator/grating'
         columns['Mono Mirror'] = '/Beamline/Monochromator/mirror'
         columns['Polarization'] = 'Beamline/Source/EPU/Polarization'
-        #columns['Comment'] = 'command'
         columns['Status'] = 'status'
 
+    # Get the meta data for all specified dict keys
     infoObj = ScanInfo(config, file,list(columns.values()))
     info = infoObj.info_dict
+
+    # generate pandas data frame to store the entries
     return pd.DataFrame(info).rename(invert_dict(columns), axis=1)
