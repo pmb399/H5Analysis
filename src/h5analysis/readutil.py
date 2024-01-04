ce079d94c48659d3cd9afb82aad8555eca38ff8e
pmb399 <web@braunpatrick.de>
2024-01-04 14:43:04 -0600

    Review and Doc "readutil"

--- src/h5analysis/readutil.py
+++ src/h5analysis/readutil.py
@@ -1,9 +1,37 @@
+# Scientific modules
 import numpy as np
 
 def detector_norm(detector_mca,norm_by):
-    """Normalize the detector data by mesh current."""
+    """Normalize the detector data by mesh current.
+    
+        Parameters
+        ----------
+        detector_mca: numpy array
+            2d data
+        norm_by: numpy array
+            1d data, with length equal to first axis of detector_mca
+
+        Returns
+        -------
+        Division of detector_mca and norm_by array
+    """
+
     return np.true_divide(detector_mca,norm_by[:, None])
 
-def stack_norm(detector_mca,norm_by):
-    """Normalize the stack data by mesh current."""
-    return np.true_divide(detector_mca,norm_by[:, None, None])
+def stack_norm(detector_stack,norm_by):
+    """Normalize the stack data by mesh current.
+    
+        Parameters
+        ----------
+        detector_stack: numpy array
+            2d data
+        norm_by: numpy array
+            1d data, with length equal to first axis of detector_stack
+
+        Returns
+        -------
+        Division of detector_mca and norm_by array
+
+    """
+    
+    return np.true_divide(detector_stack,norm_by[:, None, None])
