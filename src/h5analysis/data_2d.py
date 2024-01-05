fd4cf8fa2dedb0bf0d892ad53d59593e664a1f3d
pmb399 <web@braunpatrick.de>
2024-01-05 09:56:22 -0600

    Review and Doc "data_2d"

--- src/h5analysis/data_2d.py
+++ src/h5analysis/data_2d.py
@@ -1,91 +1,183 @@
+# Scientific modules
 import numpy as np
-import warnings
+
+# Data reader
 from .ReadData import Data
+
+# Data utils
 from .parser import parse
 from .datautil import get_roi, get_indices, mca_roi, strip_roi, stack_roi
 from .util import check_key_in_dict
 from .simplemath import apply_offset, grid_data2d
 from .readutil import detector_norm
 
+# Warnings
+import warnings
+
 def load_2d(config, file, x_stream, detector, *args, norm=False, xoffset=None, xcoffset=None, yoffset=None, ycoffset=None,grid_x=[None, None, None],grid_y=[None, None,None],norm_by=None,):
-    
+    """ Internal function to load 2d MCA data
+
+        Parameters
+        ----------
+        config: dict
+            h5 configuration
+        file: string
+            filename
+        x_stream: string
+            h5 sca key or alias of the x-stream
+        detector: string
+            alias of the MCA detector
+        *args: ints
+            scan numbers
+        kwargs
+            norm: boolean, None
+            xoffset: list of tuples
+                fitted offset (x-stream)
+            xcoffset: float
+                constant offset (x-stream)
+            yoffset: list of tuples
+                fitted offset (y-stream)
+            ycoffset: float
+                constant offset (y-stream)
+            grid_x: list
+                grid equally spaced in x with [start, stop, delta]
+            grid_y: list
+                grid equally spaced in y with [start, stop, delta]
+            norm_by: string
+                norm MCA by defined h5 key or SCA alias
+
+        Returns
+        -------
+        data: dict
+    """
+
+    # Store all data in data dict
     data = dict()
+
+    # Iterate over all scans
     for arg in args:
         
+        # reqs: Store names of the requested data streams
+        # rois: rois[stream][reqs]['req'/'roi']
         reqs = list()
         rois = dict()
 
+        # Create h5 Data object
         data[arg] = Data(config,file,arg)
         data[arg].scan = arg
         
+        # Analyse x-stream and y-stream requests with parser
+        # Get lists of requisitions
         contrib_x_stream = parse(x_stream)
         contrib_z_stream = parse(detector)
 
+        # Strip the requsitions and sort reqs and rois
         reqs, rois = strip_roi(contrib_x_stream,'x', reqs, rois)
         reqs, rois = strip_roi(contrib_z_stream,'z', reqs, rois)
 
+        # Get the data for all reqs
         all_data = data[arg].Scan(reqs)
 
         # x stream
+        # Set up an x_stream_convert in which we will replace the strings with local data variables
+        # and evaluate the expression later
         x_stream_convert = x_stream
+
         for i,x in enumerate(contrib_x_stream):
+
+            # Check if x component has ROI
             if check_key_in_dict(x,rois['x']):
+                # Check that dim(x) = 1
                 if len(np.shape(all_data[rois['x'][x]['req']])) == 1:
+                    # Check that we only have 1 ROI x-stream to reduce to dim 0
+                    if len(contrib_x_stream) != 1:
+                        raise Exception('Only one ROI x-stream supported.')
+                    # Ensure ROI is of correct type
                     if isinstance(rois['x'][x]['roi'],tuple):
+                        # x dimension will be 0 and we only obtain indices
                         dim = 0
                         xlow,xhigh = get_indices(rois['x'][x]['roi'],all_data[rois['x'][x]['req']])
                     else:
                         raise Exception('Error in specified ROI')
 
+                # Check that dim(x) = 2
                 elif len(np.shape(all_data[rois['x'][x]['req']])) == 2:
+                    # Require ROI is of correct type
                     if isinstance(rois['x'][x]['roi'],tuple):
+                        # x-dimension will be 1
                         dim = 1
+
+                        # Get indices and reduce data
                         xlow,xhigh = get_indices(rois['x'][x]['roi'],all_data[f"{rois['x'][x]['req']}_scale"])
                         x_data = mca_roi(all_data[rois['x'][x]['req']],xlow,xhigh,1,scale=all_data[f"{rois['x'][x]['req']}_scale"])
+
+                        # Add data to locals
                         locals()[f"s{arg}_val{i}_x"] = x_data
                         x_stream_convert = x_stream_convert.replace(x,f"s{arg}_val{i}_x")
                     else:
                         raise Exception('Error in specified ROI')
                 else:
                     raise Exception("Wrong x dimensions")
+            
+            # If x component has no ROI
             else:
                 if len(np.shape(all_data[x])) == 1:
+                    # len(contrix_x_stream) requirement above implicitly verifies that
+                    # we can only have multiple x components if dim=1
                     dim = 1
+
+                    # Add data to locals
                     locals()[f"s{arg}_val{i}_x"] = all_data[x]
                     x_stream_convert = x_stream_convert.replace(x,f"s{arg}_val{i}_x")
                 else:
                     raise Exception("Wrong input dimension")
-        
+
+        # Check proper dimensions for x-stream
         if not (dim==0 or dim == 1):
             raise Exception('Error defining x-stream')
         
+        # If dim_x == 1, can evaluate expression
         if dim == 1:
             # Assign the calculated result to the x_stream of the object in data dict
             data[arg].x_data = eval(x_stream_convert)
                 
         # z-stream
+        # Set up an z_stream_convert in which we will replace the strings with local data variables
+        # and evaluate the expression later
         z_stream_convert = detector
+
         for i,z in enumerate(contrib_z_stream):
+            # Check if requisition has ROIs
             if check_key_in_dict(z,rois['z']):
+                # Need to have a STACK-ROI for this, thus require dim(z)=3
                 if len(np.shape(all_data[rois['z'][z]['req']])) == 3:
+                    # Check that ROI is appropriate
                     if isinstance(rois['z'][z]['roi'],dict):
+                        # Get ROI indices
                         idxLow1,idxHigh1 = get_indices(rois['z'][z]['roi']['roi_list'][0],all_data[f"{rois['z'][z]['req']}_scale1"])
                         idxLow2,idxHigh2 = get_indices(rois['z'][z]['roi']['roi_list'][1],all_data[f"{rois['z'][z]['req']}_scale2"])
 
+                        # Reduce STACK once along specified scale axis if dim(x) = 1
                         if dim == 1:
+                            # Calculate resulting 2d data
                             z_data = stack_roi(all_data[f"{rois['z'][z]['req']}"],None,None,idxLow1,idxHigh1,idxLow2,idxHigh2,rois['z'][z]['roi']['roi_axes'],scale1=all_data[f"{rois['z'][z]['req']}_scale1"],scale2=all_data[f"{rois['z'][z]['req']}_scale2"])
+                            
+                            # Ensure result is of correct dimensions
                             if len(np.shape(z_data)) == 2:
                                 # Add data to locals
                                 locals()[f"s{arg}_val{i}_z"] = z_data
                                 z_stream_convert = z_stream_convert.replace(z,f"s{arg}_val{i}_z")
 
+                                # Determine the remaining axis
                                 integration_axes = rois['z'][z]['roi']['roi_axes']
                                 all_axes = {1,2}
                                 y_axis_raw = all_axes - set(integration_axes)
-                                if not len(list(y_axis_raw)) == 1:
+                                if not len(list(y_axis_raw)) == 1: # Can only have exactly one axis as per dimensionality requirements
                                     raise Exception('Error determining proper integration axes')
                                 else:
-                                    y_axis = list(y_axis_raw)[0]
+                                    y_axis = list(y_axis_raw)[0] # convert to single element (from set to list, then slice)
+
+                                    # Set the remaining (second) axis as y-data
                                     if y_axis == 1:
                                         data[arg].y_data = all_data[f"{rois['z'][z]['req']}_scale1"][idxLow1:idxHigh1]
                                     elif y_axis == 2:
@@ -95,22 +187,29 @@ def load_2d(config, file, x_stream, detector, *args, norm=False, xoffset=None, x
                             else:
                                 raise Exception('Data dimensionality incompatible with loader. Check integration axes.')
                             
+                        # Reduce STACK once along independent axis if dim(x) == 0
                         elif dim == 0:
                             if not isinstance(xlow,type(None)) and not isinstance(xhigh,type(None)):
                                 if xlow > xhigh:
                                     warnings.warn("xlow>xhigh.\nEither select a single value or [None:None] to integrate over all.\nThis most likely happens because the chosen x-stream is not monotonic.")
                             
+                            # Set the integration axis
                             integration_axes = tuple([0])
 
+                            # Ensure no other integration axes are specified
                             if not len(rois['z'][z]['roi']['roi_axes']) == 0:
                                 raise Exception('Inproper integration axes. You may not specify integration axes on the image.')
 
+                            # Calculate resulting 2d data
                             z_data = stack_roi(all_data[f"{rois['z'][z]['req']}"],xlow,xhigh,idxLow1,idxHigh1,idxLow2,idxHigh2,integration_axes,scale1=all_data[f"{rois['z'][z]['req']}_scale1"],scale2=all_data[f"{rois['z'][z]['req']}_scale2"])
+                            
+                            # Ensure proper dimensions
                             if len(np.shape(z_data)) == 2:
                                 # Add data to locals
                                 locals()[f"s{arg}_val{i}_z"] = z_data
                                 z_stream_convert = z_stream_convert.replace(z,f"s{arg}_val{i}_z")
 
+                                # Assign the scales as x/y
                                 data[arg].x_data = all_data[f"{rois['z'][z]['req']}_scale1"][idxLow1:idxHigh1]
                                 data[arg].y_data = all_data[f"{rois['z'][z]['req']}_scale2"][idxLow2:idxHigh2]
 
@@ -124,19 +223,25 @@ def load_2d(config, file, x_stream, detector, *args, norm=False, xoffset=None, x
                         raise Exception("Error in specified ROI")
                 else:
                     raise Exception("Wrong z dimensions")
+                
+            # There are no ROI specified
             else:
+                # Need to require MCA data with dim = 2
                 if len(np.shape(all_data[z])) == 2:
+                    # Set the corresponding scale as y-data
                     data[arg].y_data = all_data[f"{z}_scale"]
+
+                    # Add data to locals
                     locals()[f"s{arg}_val{i}_z"] = all_data[z]
                     z_stream_convert = z_stream_convert.replace(z,f"s{arg}_val{i}_z")
+
                 else:
                     raise Exception("Wrong input dimension")
-
 
         # Apply x offset
         data[arg].x_data = apply_offset(data[arg].x_data, xoffset, xcoffset)
 
-        # Assign the calculated result to the y_stream of the object in data dict
+        # Apply y offset
         data[arg].y_data = apply_offset(data[arg].y_data, yoffset, ycoffset)
 
         # Assign the calculated result to the z_stream of the object in data dict
@@ -153,6 +258,7 @@ def load_2d(config, file, x_stream, detector, *args, norm=False, xoffset=None, x
         if norm == True:
             data[arg].detector = data[arg].detector/np.max(data[arg].detector)
 
+        # Grid data to image
         xmin, xmax, ymin, ymax, new_x, new_y, new_z = grid_data2d(data[arg].x_data, data[arg].y_data, data[arg].detector, grid_x=grid_x,grid_y=grid_y)
         data[arg].xmin = xmin
         data[arg].xmax = xmax
