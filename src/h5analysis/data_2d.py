3658347ca5a2c06107b0f4c8e8d9960d111b4a4d
pmb399 <web@braunpatrick.de>
2024-04-08 17:08:47 -0600

    improve labelling in 2d data loader

--- src/h5analysis/data_2d.py
+++ src/h5analysis/data_2d.py
@@ -124,6 +124,8 @@ def load_2d(config, file, x_stream, detector, *args, norm=False, xoffset=None, x
                         # Add data to locals
                         locals()[f"s{arg}_val{i}_x"] = all_data[x]
                         x_stream_convert = x_stream_convert.replace(x,f"s{arg}_val{i}_x")
+                        if config.h5dict[x]['x_label'] != None:
+                                    data[arg].xlabel = config.h5dict[x]['x_label']
                     else:
                         raise Exception(f"Wrong input dimension: {x}")
 
