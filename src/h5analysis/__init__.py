c4943773acca36be30003d2947cda1db5c85ac79
pmb399 <web@braunpatrick.de>
2024-05-02 12:15:34 -0600

    update version management and CI

--- src/h5analysis/__init__.py
+++ src/h5analysis/__init__.py
@@ -1,4 +1,10 @@
 """Library to analyse, plot, and export data stored in HDF5 files."""
 
-__version__ = '0.0.62'
+try:
+    from ._version import version as __version__
+    from ._version import version_tuple
+except ImportError:
+    __version__ = "unknown version"
+    version_tuple = (0, 0, "unknown version")
+
 __author__ = 'Patrick Braun'
